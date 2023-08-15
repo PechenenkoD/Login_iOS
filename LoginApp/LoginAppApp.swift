d49e46eb8093ee4a1849d97091712088ee9fd336
d.pechenenko <pechenenkod@gmail.com>
2023-08-15 12:01:47 +0300

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Dmitro Pechenenko on 15.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
